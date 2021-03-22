888c9ebc75ac16fd874118027dcf1658cebb5ca4
MyongjinKim <64261919+MyongjinKim@users.noreply.github.com>
2021-03-22 18:34:02 +0900

    Update renamer.py

--- renamer.py
+++ renamer.py
@@ -11,18 +11,25 @@ for name in file_names:        ##you can put the number in order in your class
     os.rename(src,dst)
     i+=1
 
-##example
+"""example
 
-#import os
-#file_path = ~/Downloads/train_images
-#file_names =os.listdir(file_path)  
-#
-#i=1
-#
-#for name in file_names:       
-#    src = os.path,join(file_path, name)
-#    dst = 'cup' + '.jpg' 
-#    dst = os.path.join(file_path, dst)
-#    os.rename(src,dst)
-#    i+=1
+import os
 
+path='/home/mooin/yolov5/runs/detect/Side/'
+
+file_list = os.listdir(path)
+
+for num in range(len(file_list)):
+    file_path = os.path.join(path,file_list[num])
+    file_path = file_path + '/crop'
+    file_names =os.listdir(file_path)
+    folder_name = file_list[num]
+    i=1
+    for name in file_names:   
+        src = os.path.join(file_path,name)
+        dst = folder_name + '_' + name.split('-')[0] + str(i) + '.jpg'
+        dst = os.path.join(file_path,dst)
+        os.rename(src,dst)
+        i+=1
+
+"""
